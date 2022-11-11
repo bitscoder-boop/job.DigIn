44cca8ee8f8c3964ce16be998cb2c669463bb307
bitscoder-boop <regdipesh113@gmail.com>
2022-11-11 20:12:32 +0545

    Test directory created

--- sites/jobAxle/scrapper.py
+++ sites/jobAxle/scrapper.py
@@ -2,11 +2,17 @@ import requests
 
 url = "https://jobaxle.com/search"
 
-def search(search_text: str, category_id = '' , job_location_id = ''):
-    payload='search_text={}&cat_id={}&job_location_id={}'
-    response = requests.post(url, data=payload.format(search_text, category_id, job_location_id))
+
+def search(search_text: str, category_id='', job_location_id=''):
+    payload = 'search_text={}&cat_id={}&job_location_id={}'
+    response = requests.post(url, data=payload.format(
+        search_text, category_id, job_location_id))
     if response.status_code == 200:
-        print(response.text)
+        return response.text
+
+
+def format_response(response):
+    return response
 
 
 if __name__ == '__main__':
